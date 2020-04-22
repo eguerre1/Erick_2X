5fe6016b92613b63fc2ce66b58d7da927dc6c0bf
Erick Guerrero Illanez <eguerre1@uncc.edu>
2020-04-22 00:08:12 +0000

    Added Author Designation to articles

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
+  
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
   resources :users
   get 'welcome/index'
   
-  resources :articles do
-    resources :comments
-    
-  resources :sessions, only: [:new, :create, :destroy]
+   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  resources :articles do
+    resources :comments
   end
   
   root 'welcome#index'
