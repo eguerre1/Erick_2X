972385aacb1f5a29185d69e1dfc08ce9cadb3e22
Erick Guerrero Illanez <eguerre1@uncc.edu>
2020-04-23 18:39:39 +0000

    Fixed RegEx The Kendall anomaly

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :articles
     
-    VALID_EMAIL_REGEX = /A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     before_save { self.email = email.downcase }
     
     validates :name,
