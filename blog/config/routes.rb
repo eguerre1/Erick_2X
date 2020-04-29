cc892e4a4fcc5e6d44b75f86e6e84dcdb17d174e
Faisal Salad <majorsalad@gmail.com>
2020-04-29 18:57:26 +0000

    Title filter

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
+  get 'sortAlphabetical', to: 'articles#sortAlphabetical', as: 'sortAlphabetical'
   resources :articles do
     resources :comments
     resources :stars
